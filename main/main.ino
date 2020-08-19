12698bc258a68c939d8b80bdd1db6ed4a301b9ac
Arnav Bansal <arnav.bansal95@gmail.com>
2020-08-19 17:11:39 +0200

    Added basic message sniffing

--- main/main.ino
+++ main/main.ino
@@ -27,5 +27,26 @@ void setup()
 
 void loop()
 {
-    // Do Nothing
+    if(CAN_Init)
+    {
+        unsigned char len = 0;
+        unsigned char buf[8];
+        if (CAN_MSGAVAIL == CAN.checkReceive())     // check if data coming
+        {         
+            CAN.readMsgBuf(&len, buf);              // read data,  len: data length, buf: data buf
+
+            unsigned long canId = CAN.getCanId();
+
+            Serial.println("-----------------------------");
+            Serial.print("Get data from ID: 0x");
+            Serial.println(canId, HEX);
+
+            for (int i = 0; i < len; i++)           // print the data 
+            { 
+                Serial.print(buf[i], HEX);
+                Serial.print("\t");
+            }
+            Serial.println();
+        }
+    }
 }
